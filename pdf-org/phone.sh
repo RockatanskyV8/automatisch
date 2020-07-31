c39ac7cdfbb666413954c73f9e052a89dee2f600
lucas <luccasanttoz@gmail.com>
2020-07-30 23:44:59 -0300

    exibe mensagem de erro caso arquivo não exista

--- pdf-org/phone.sh
+++ pdf-org/phone.sh
@@ -3,31 +3,31 @@
 COMPROVANTES="$HOME/Documentos/.Sync/ComprovantesBB"
 COMP_DEST="$HOME/Documentos/PDF/ComprovantesBB"
 
-[ "$(ls -A $COMPROVANTES/*.pdf)" ] && mv $COMPROVANTES/*.pdf $COMP_DEST
+[[ -f $COMPROVANTES/*.pdf ]] && mv $COMPROVANTES/*.pdf $COMP_DEST || echo "Sem Comprovantes em $COMPROVANTES"
 
 DOWNLOADSPDF="$HOME/Downloads/.Sync/PDF"
 DWNLPDF_DEST="$HOME/Documentos/PDF/"
 
-[ "$(ls -A $DOWNLOADSPDF/*.pdf)" ] && mv $DOWNLOADSPDF/*.pdf $DWNLPDF_DEST
+[[ -f $DOWNLOADSPDF/*.pdf ]] && mv $DOWNLOADSPDF/*.pdf $DWNLPDF_DEST || echo "Sem PDF's $DOWNLOADSPDF"
 
 WPP="$HOME/Sync/Whatsapp/Media"
 
 VIDEOS="$HOME/Vídeos/WPP"
-[ "$(ls -A $WPP/*Video/*.mp4)" ] &&  mv $WPP/*Video/*.mp4 $VIDEOS
-[ "$(ls -A $WPP/*Video/Sent/*.mp4)" ] &&  mv $WPP/*Video/Sent/*.mp4 $VIDEOS
+[[ -f $WPP/*Video/*.mp4 ]] &&  mv $WPP/*Video/*.mp4 $VIDEOS || echo "Nenhum vídeo em $WPP"
+[[ -f $WPP/*Video/Sent/*.mp4 ]] &&  mv $WPP/*Video/Sent/*.mp4 $VIDEOS || echo "Nenhum vídeo $WPP"
 
 IMAGENS="$HOME/Imagens/WPP"
-[ "$(ls -A $WPP/*Images/*.jpg)" ] && mv $WPP/*Images/*.jpg $IMAGENS
-[ "$(ls -A $WPP/*Images/*.jpeg)" ] && mv $WPP/*Images/*.jpeg $IMAGENS
-[ "$(ls -A $WPP/*Images/Sent/*.jpg)" ] && mv $WPP/*Images/Sent/*.jpg $IMAGENS
-[ "$(ls -A $WPP/*Images/Sent/*.jpeg)" ] && mv $WPP/*Images/Sent/*.jpeg $IMAGENS
-[ "$(ls -A $WPP/*Gifs/*.gif)" ] && mv $WPP/*Gifs/*.gif $IMAGENS
-[ "$(ls -A $WPP/*Gifs/Sent/*.gif)" ] && mv $WPP/*Gifs/Sent/*.gif $IMAGENS
+[[ -f $WPP/*Images/*.jpg ]] && mv $WPP/*Images/*.jpg $IMAGENS || echo "Nenhum jpg em $WPP"
+[[ -f $WPP/*Images/*.jpeg ]] && mv $WPP/*Images/*.jpeg $IMAGENS || echo "Nenhum jpeg em $WPP"
+[[ -f $WPP/*Images/Sent/*.jpg ]] && mv $WPP/*Images/Sent/*.jpg $IMAGENS || echo "Nenhum jpg em $WPP"
+[[ -f $WPP/*Images/Sent/*.jpeg ]] && mv $WPP/*Images/Sent/*.jpeg $IMAGENS || echo "Nenhum jpeg em $WPP"
+[[ -f $WPP/*Gifs/*.gif ]] && mv $WPP/*Gifs/*.gif $IMAGENS || echo "Nenhum gif em $WPP"
+[[ -f $WPP/*Gifs/Sent/*.gif ]] && mv $WPP/*Gifs/Sent/*.gif $IMAGENS || echo "Nenhum gif em $WPP"
 
 DOCUMENTOS="$HOME/Documentos/WPP"
-[ "$(ls -A $WPP/*Documents/*.pdf)" ] && mv $WPP/*Documents/*.pdf $DOCUMENTOS
+[[ -f $WPP/*Documents/*.pdf ]] && mv $WPP/*Documents/*.pdf $DOCUMENTOS || echo "Nenhum PDF em $WPP"
 
 AUDIO="$HOME/Música/WPP"
-[ "$(ls -A $WPP/*Audio/*.opus)" ] && mv $WPP/*Audio/*.opus "$AUDIO/audio"
-[ "$(ls -A $WPP/*Voice*/)" ] && mv $WPP/*Voice*/* "$AUDIO/msgvoice"
+[[ -f $WPP/*Audio/*.opus ]] && mv $WPP/*Audio/*.opus "$AUDIO/audio" || echo "Nenhum audio em $WPP"
+[[ -f $WPP/*Voice*/ ]] && mv $WPP/*Voice*/* "$AUDIO/msgvoice" || echo "Nenhuma mensagem de voz em $WPP"
 
